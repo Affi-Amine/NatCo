bd797bc5f478c998ce84570fc2a9e99ea8b05564
Amine <affi.amin.work@gmail.com>
2024-11-21 16:15:13 +0100

    Oc section setup finished (minor things to change after verification)

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,54 @@ import Footer from "@/components/Footer";
 import { Hero } from "@/components/Hero";
 import ImageTransitionSection from "@/components/ImageTransition";
 import Slider from "@/components/Slider";
+import { AnimatedTestimonials } from "@/components/ui/animated-testimonials";
 import { div } from "framer-motion/client";
 import Image from "next/image";
+
+const ocTeam = [
+  {
+    quote: "This is an amazing experience! Highly recommended.",
+    name: "John Doe",
+    designation: "Software Engineer",
+    src: "/oc/i1.jpg", 
+  },
+  {
+    quote: "I loved the service, and the team was incredible!",
+    name: "Jane Smith",
+    designation: "Product Manager",
+    src: "/oc/i2.jpg",
+  },
+  {
+    quote: "Exceptional quality and attention to detail.",
+    name: "Alice Johnson",
+    designation: "Designer",
+    src: "/oc/i3.jpg",
+  },
+  {
+      quote: "This is an amazing experience! Highly recommended.",
+      name: "John Doe",
+      designation: "Software Engineer",
+      src: "/oc/i4.jpg", 
+    },
+    {
+      quote: "I loved the service, and the team was incredible!",
+      name: "Jane Smith",
+      designation: "Product Manager",
+      src: "/oc/i5.jpg",
+    },
+    {
+      quote: "Exceptional quality and attention to detail.",
+      name: "Alice Johnson",
+      designation: "Designer",
+      src: "/oc/i6.jpg",
+    },
+    {
+      quote: "Exceptional quality and attention to detail.",
+      name: "Alice Johnson",
+      designation: "Designer",
+      src: "/oc/i7.jpg",
+    },
+];
 
 
 export default function Home() {
@@ -15,6 +61,7 @@ export default function Home() {
       
       <Definition />
       <Slider />
+      <AnimatedTestimonials testimonials={ocTeam} autoplay={true} />
       <Footer />
     </div>
   );
