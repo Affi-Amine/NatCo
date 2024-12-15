98821008f575915ebe9a686e876e3c92368c8a23
Amine <affi.amin.work@gmail.com>
2024-12-15 19:20:59 +0100

    opening registration round2

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,10 +1,10 @@
-import RegistrationClosed from '@/components/RegistrationClosed'
+import RegistrationForm from '@/components/RegistrationForm'
 import React from 'react'
 
 const page = () => {
   return (
-    <RegistrationClosed />
-    //<RegistrationForm />
+    //<RegistrationClosed />
+    <RegistrationForm />
   )
 }
 
